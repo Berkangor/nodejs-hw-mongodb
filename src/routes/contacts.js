6a24fd68cc34150b31058e6621ad5e52f5b8113b
berkangormus <berkangormus@outlook.com>
2025-09-03 23:57:03 +0300

    hata düzeltme 5

--- src/routes/contacts.js
+++ src/routes/contacts.js
@@ -1,5 +1,7 @@
 import express from 'express';
-import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+import mongoose from 'mongoose';
+import createHttpError from 'http-errors';
+
 import {
   getAllContactsController,
   getContactByIdController,
@@ -7,16 +9,30 @@ import {
   updateContactController,
   deleteContactController,
 } from '../controllers/contacts.js';
+
+import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 import { validateBody } from '../middleware/validateBody.js';
 import { isValidId } from '../middleware/isValidId.js';
 import { createContactSchema, updateContactSchema } from '../validation/contactsSchemas.js';
 
 const router = express.Router();
 
+// :contactId param kontrolü (geçersiz ObjectId ise 400)
+router.param('contactId', (req, res, next, id) => {
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return next(createHttpError(400, 'Invalid contact ID format'));
+  }
+  next();
+});
+
+// CRUD
 router.get('/', ctrlWrapper(getAllContactsController));
 router.get('/:contactId', isValidId(), ctrlWrapper(getContactByIdController));
 router.post('/', validateBody(createContactSchema), ctrlWrapper(createContactController));
 router.patch('/:contactId', isValidId(), validateBody(updateContactSchema), ctrlWrapper(updateContactController));
 router.delete('/:contactId', isValidId(), ctrlWrapper(deleteContactController));
+
+// (Opsiyonel) 405 Method Not Allowed örneği:
+// router.all('/', (_req, res) => res.status(405).json({ message: 'Method Not Allowed' }));
 
 export default router;
