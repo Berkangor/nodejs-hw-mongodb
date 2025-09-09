441bf9f292513c2c606e614766a754337d032c16
berkangormus <berkangormus@outlook.com>
2025-09-09 12:00:56 +0300

    düzeltme

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -4,25 +4,22 @@ import {
   registerUserController,
   loginUserController,
   refreshUserController,
-  logoutUserController,
-  logoutUserController,
+  logoutUserController,      
   sendResetEmailController,
   resetPwdController,
 } from '../controllers/auth.js';
-import { registerUserSchema, loginUserSchema, sendResetEmailSchema, resetPwdSchema } from '../validation/auth.js';
+import {
+  registerUserSchema,
+  loginUserSchema,
+  sendResetEmailSchema,
+  resetPwdSchema,
+} from '../validation/auth.js';
 
 const router = Router();
 
-router.post(
-  '/register',
-  validateBody(registerUserSchema),
-  registerUserController,
-);
-
+router.post('/register', validateBody(registerUserSchema), registerUserController);
 router.post('/login', validateBody(loginUserSchema), loginUserController);
-
 router.post('/refresh', refreshUserController);
-
 router.post('/logout', logoutUserController);
 
 router.post('/send-reset-email', validateBody(sendResetEmailSchema), sendResetEmailController);
